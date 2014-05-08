003c506fb68c1e104675005d17c3c9f498e0d7f6
Andrew Peck <anp@ucla.edu>
2014-05-08 10:56:15 -0700

    Update JTAGUtils with some useful functions

--- JTAGUtils.py
+++ JTAGUtils.py
@@ -6,110 +6,89 @@ def BitToByteLen(size):
     result = ((size - 1) // 8)+1;
     return(result)
 
-def AdjustStrSize(instream, size):
-    hexsize = BitToHexLen(size);
-    result = instream;
-    while (len(result) < hexsize)
-        result = '0' + result
+#def AdjustStrSize(instream, size):
+#    hexsize = BitToHexLen(size);
+#    result = instream;
+#    while (len(result) < hexsize)
+#        result = '0' + result
+#
+#    if (length(result) > hexsize):
+#        Delete(result, 0, len(result)-hexsize);
+#        SetLength(Result,hexsize);
+#
+#    mask := Trunc(IntPower(2, 4-(hexsize*4-size)))-1;
+#    SetLength(tmp,1);
+#    tmp := IntToHex(StrToInt('$'+Copy(Result,0,1)) and mask,1);
+#    Result := UpperCase(tmp + Copy(Result, 2, Length(Result)-1));
 
-    if (length(result) > hexsize):
-        Delete(result, 0, len(result)-hexsize);
-        SetLength(Result,hexsize);
+#def ReverseStream(instream, size):
+#var
+#    tmpstr := AdjustStrSize(instream, size);
+#    SetLength(arBit,size);
+#    for j in range(size): 
+#        for i:=BitToHexLen(size) downto 1 do
+#        begin
+#            tmp := Copy(tmpstr, i ,1);
+#            tmp := IntToHex(RevHexDigit(StrToInt('$'+tmp)),1);
+#            result = result + tmp ;
 
-    mask := Trunc(IntPower(2, 4-(hexsize*4-size)))-1;
-    SetLength(tmp,1);
-    tmp := IntToHex(StrToInt('$'+Copy(Result,0,1)) and mask,1);
-    Result := UpperCase(tmp + Copy(Result, 2, Length(Result)-1));
+def RevHexDigit(digit):
+    FlipHalfByte(digit)
 
-def ReverseStream(instream, size):
-var
-    tmpstr := AdjustStrSize(instream, size);
-    SetLength(arBit,size);
-    for j in range(size): 
-        for i:=BitToHexLen(size) downto 1 do
-        begin
-            tmp := Copy(tmpstr, i ,1);
-            tmp := IntToHex(RevHexDigit(StrToInt('$'+tmp)),1);
-            result = result + tmp ;
+def FlipByte(value): 
+    result = 0;
+    for i in range(8): 
+        result = result | (((value >> i) & 0x1) << (7-i))
+    return(result)
 
-function RevHexDigit(const digit: integer): integer;
-var
-    i	: integer;
-begin
-    Result := 0;
-    for i:=0 to 3 do Result := Result or (((digit shr i) and $1) shl (3-i));
-end;
+def FlipHalfByte(value):
+    result = 0
+    for i in range(4): 
+        result = result | (((value  >> i) & 0x1) << (3-i))
+    return(result)
 
-function FlipByte(const value: byte):byte;
-var
-    i : byte;
-begin
-    Result := 0;
-    for i:=0 to 7 do
-        Result := Result or (((value  shr i) and $1) shl (7-i));
-end;
+def FlipWord(word):
+    result = 0
+    for i in range(16): 
+        result = result | (((word >> i) & 0x1) << (15-i));
+    return(result)
 
-function FlipHalfByte(const value: byte):byte;
-var
-    i : byte;
-begin
-    Result := 0;
-    for i:=0 to 3 do
-        Result := Result or (((value  shr i) and $1) shl (3-i));
-end;
+def FlipLongword(longword):
+    result = 0
+    for i in range(32): 
+        result = result | (((word >> i) & 0x1) << (31-i));
+    return(result)
 
-function FlipWord(const value: word): word;
-var
-    i : byte;
-begin
-    Result := 0;
-    for i:=0 to 15 do
-        Result := Result or (((value  shr i) and $1) shl (15-i));
-end;
-
-function Flip(const value: longword): longword;
-var
-    i : byte;
-begin
-    Result := 0;
-    for i:=0 to 31 do
-        Result := Result or (((value  shr i) and $1) shl (31-i));
-end;
-
-function FirstDelimiter(const Delimiters, S: string): Integer;
-var
-    P: PChar;
-begin
-    //  Result := Length(S);
-    Result := 1;
-    P := PChar(Delimiters);
-    while Result <= Length(S) do
-    begin
-        if (S[Result] <> #0) and (StrScan(P, S[Result]) <> nil) then
-{$IFDEF MSWINDOWS}
-            if (ByteType(S, Result) = mbTrailByte) then
-            begin
-                Dec(Result);
-                Exit;
-            end
-            else
-                Exit;
-{$ENDIF}
-{$IFDEF LINUX}
-            begin
-                if (ByteType(S, Result) <> mbTrailByte) then
-                    Exit;
-                //	  Dec(Result);
-                while ByteType(S, Result) = mbTrailByte do Dec(Result);
-                Exit;
-            end;
-{$ENDIF}
-            Inc(Result);
-        end;
-        Result := 0;
-    end;
-
-
-end.
-
+#function FirstDelimiter(const Delimiters, S: string): Integer;
+#var
+#    P: PChar;
+#begin
+#    //  Result := Length(S);
+#    Result := 1;
+#    P := PChar(Delimiters);
+#    while Result <= Length(S) do
+#    begin
+#        if (S[Result] <> #0) and (StrScan(P, S[Result]) <> nil) then
+#{$IFDEF MSWINDOWS}
+#            if (ByteType(S, Result) = mbTrailByte) then
+#            begin
+#                Dec(Result);
+#                Exit;
+#            end
+#            else
+#                Exit;
+#{$ENDIF}
+#{$IFDEF LINUX}
+#            begin
+#                if (ByteType(S, Result) <> mbTrailByte) then
+#                    Exit;
+#                //	  Dec(Result);
+#                while ByteType(S, Result) = mbTrailByte do Dec(Result);
+#                Exit;
+#            end;
+#{$ENDIF}
+#            Inc(Result);
+#        end;
+#        Result := 0;
+#    end;
 
